ece5523a38f0af90167630cca7cef6a2bbca447d
Lucas Bilk Matos <lucas.matos@petlove.com.br>
2023-03-05 22:59:45 -0300

    more tests

--- spec/requests/vacation_periods_spec.rb
+++ spec/requests/vacation_periods_spec.rb
@@ -5,30 +5,29 @@ require 'rails_helper'
 RSpec.describe 'VacationPeriods', type: :request do
   let!(:collaborator) { create(:collaborator) }
 
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/vacation_periods/index'
-      expect(response).to have_http_status(:success)
-    end
-  end
-
   describe 'POST /create' do
     let(:start_date) { DateTime.now }
     let(:end_date) { start_date + 10.days }
     let(:params) do
-      {
-        start_date: start_date,
-        end_date: end_date,
-        collaborator_id: collaborator.id
+      { 
+        vacation_period: {
+          'start_date(1i)': 2023,
+          'start_date(2i)': 3,
+          'start_date(3i)': 5,
+          'end_date(1i)': 2023,
+          'end_date(2i)': 3,
+          'end_date(3i)': 25
+        },
+        collaborator: {
+          id: collaborator.id
+        }
       }
     end
 
     it 'returns http success' do
-      # expect(Vacations::AssignService).to receive(:call)
+      post('/vacation_periods', params: params)
 
-      post('/vacation_periods/create', params: params)
-
-      # expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:found)
     end
   end
 end
